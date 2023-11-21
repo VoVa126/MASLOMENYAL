4cda9a16f9040c7fdd68e53a3c9b85e4bc912a6c
VoVa126 <144896032+VoVa126@users.noreply.github.com>
2023-11-21 23:08:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 function calc() {
-    let type_tovara = document.getElementById("tovar");
-    let count = document.getElementById("count");
-    let result = document.getElementById("result");
-    let price = 0;
-    price += parseInt(type_tovara.options[type_tovara.selectedIndex].value);
-    if(count.value>0)
-    price = parseInt(count.value) * price;
-    else alert("Укажите верное количество");
-    result.innerHTML = price;
+        //ссылка на элемент Select (Тип)
+        let type_tovara = document.getElementById("tovar");
+        //ссылка на элемент input (Кол-во)
+        let count = document.getElementById("count");
+        //ссылка на элемент span, в него пишем стоимость
+        let result = document.getElementById("result");
+        let price = 0;
+        price += parseInt(type_tovara.options[type_tovara.selectedIndex].value);
+        if(count.value>0)
+        price = parseInt(count.value) * price;
+        else alert("Укажите верное количество");
+        result.innerHTML = price;
 }
 function sbros()
 {
-    let result = document.getElementById("result");
-    let price=0;
-    result.innerHTML=price;
+        let result = document.getElementById("result");
+        let price=0;
+        result.innerHTML=price;
 }
 window.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM fully loaded and parsed");
-    let b = document.getElementById("button");
-    let c = document.getElementById("button2");
-    
-    b.addEventListener("click", calc);
-    c.addEventListener("click", sbros);
+        console.log("DOM fully loaded and parsed");
+        let b = document.getElementById("button");
+        let c = document.getElementById("button2");
+        
+        b.addEventListener("click", calc);
+        c.addEventListener("click", sbros);
 });
